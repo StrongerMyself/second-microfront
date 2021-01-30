c71eed02b93ed7d61883da36b0e5eac3e033d444
Михаил <msolovyev@itsumma.ru>
2021-01-30 15:40:42 +0800

    clean code

--- packages/server/src/registration.js
+++ packages/server/src/registration.js
@@ -2,20 +2,16 @@ const path = require('path')
 const express = require('express')
 const rerender = require('./rerender')
 
-module.exports = (app) => (name, _pathPackage) => {
-  const pathPackage = path.resolve(__dirname, _pathPackage) 
-  const pathManifest = path.resolve(pathPackage, 'manifest.json') 
-  const manifest = require(pathManifest)
-  const pathComponent = path.resolve(pathPackage, manifest['server.js'])
-  const component = require(pathComponent)
-  const manifestStatic = Object.keys(manifest).map(keyFile => {
-    const pathStatic = `/static/${name}/${manifest[keyFile]}`
-    const pathFile = path.resolve(pathPackage, manifest[keyFile])
+module.exports = (app) => (name, _pathApp) => {
+  const pathApp = path.resolve(__dirname, _pathApp) 
+  const manifest = require(path.resolve(pathApp, 'manifest.json'))
+  const component = require(path.resolve(pathApp, manifest['server.js']))
+  const manifestStatic = Object.keys(manifest).reduce((acum, key) => {
+    const pathStatic = `/static/${name}/${manifest[key]}`
+    const pathFile = path.resolve(pathApp, manifest[key])
     app.use(pathStatic, express.static(pathFile))
-    return [keyFile, pathStatic]
-  }).reduce((acc, [key, value]) => {
-    acc[key] = value
-    return acc
+    acum[key] = pathStatic
+    return acum  
   }, {})
   app.use(`/render/${name}`, rerender(name, manifestStatic, component))
 }
--- packages/server/src/server.js
+++ packages/server/src/server.js
@@ -3,19 +3,13 @@ const path = require('path')
 const express = require('express')
 const cookieParser = require('cookie-parser')
 
-const registration = require('./registration')
-// const site = require('./controllers/site')
-
 const app = express()
 app.disable('x-powered-by')
 app.use(cookieParser())
-// app.use('/static', express.static(`${__dirname}/../build`))
-// app.use('/public', express.static(`${__dirname}/../public`))
 
-// app.use('/admin', admin)
-// app.use('/*', site)
+const useApp = require('./registration')(app)
 
-registration(app)('app-filters', '../../app-filters/dist/')
+useApp('app-filters', '../../app-filters/dist/')
 
 const port = process.env.SERVER_PORT || 1234
 app.listen(port, () => {
