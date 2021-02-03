4256f172cf0449a2b533d231fe973c87bc5a73bc
Михаил <msolovyev@itsumma.ru>
2021-02-04 01:53:00 +0800

    simple server

--- packages/server/src/server.js
+++ packages/server/src/server.js
@@ -1,17 +1,35 @@
 const path = require('path')
-
 const express = require('express')
 const cookieParser = require('cookie-parser')
+const React = require('react')
+const ReactDOM = require('react-dom/server')
 
 const app = express()
 app.disable('x-powered-by')
 app.use(cookieParser())
 
-const useApp = require('./registration')(app)
+const registerApp = (name, pathApp) => {
+  const manifest = require(`${pathApp}/dist/manifest.json`)
+  const component = require(`${pathApp}/dist/${manifest['server.js']}`)
+  app.use(`/render/${name}`, renderApp(name, manifest, component))
+  Object.keys(manifest)
+    .filter(key => key.indexOf('server') !== 0)
+    .map((key) => {
+      const routeStatic = `/static/${name}/${manifest[key]}`
+      const pathFile = path.resolve(pathApp, manifest[key])
+      app.use(routeStatic, express.static(pathFile))
+    })
+}
 
-useApp('catalog-filters', '../../catalog-filters/dist/')
+const renderApp = (name, manifest, component) => async (req, res) => {
+  const markup = ReactDOM.renderToString(React.createElement(component))
+  res.json({ name, markup, manifest })
+}
 
-const port = process.env.SERVER_PORT || 3001
-app.listen(port, () => {
-    console.log(`Listening on port ${port}...`)
+registerApp('catalog-filters', '../../../packages/catalog-filters')
+
+const PORT = process.env.SERVER_PORT || 3001
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}...`)
 })
